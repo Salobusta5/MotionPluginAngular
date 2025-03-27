dcd1a7bbd67f7ef2f6ddc59482b5b8eb064c6abc
aliii.sss2333 <aliii.sss2333@gmail.com>
2025-03-27 04:18:37 +0000

    decimo

--- src/app/components/motion/motion.component.ts
+++ src/app/components/motion/motion.component.ts
@@ -12,6 +12,8 @@ export class MotionComponent implements OnInit, OnDestroy {
   
   stepCount: number = 0;
   isStepDetected: boolean = false;
+  // Flag to control whether the step counter should increment
+  isCounting: boolean = true;
 
   constructor(private motionS: MotionService, private cdRef: ChangeDetectorRef) {}
 
@@ -19,16 +21,21 @@ export class MotionComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.motionS.startMotionDetection(async (data: MotionData) => {
-      console.log('Motion data received:', data);
+      console.log('startMotionDetection callback executed');
         this.motionData = data;
-        this.stepCount = data.stepCount || 0; // Use the stepCount from the data or default to 0
-
+        // Only update the stepCount if isCounting is true
+        if (this.isCounting) {
+            this.stepCount = data.stepCount || 0; // Use the stepCount from the data or default to 0
+        }
+    
         if (data.isStepDetected) {
-          this.isStepDetected = true;
-          this.cdRef.detectChanges(); // Manually trigger change detection
-          await this.delay(500); // Wait for 500 milliseconds
-          this.isStepDetected = false;
-          this.cdRef.detectChanges(); // Manually trigger change detection
+            this.isStepDetected = true;
+            this.cdRef.detectChanges(); // Manually trigger change detection
+            await this.delay(500); // Wait for 500 milliseconds
+            this.isStepDetected = false;
+            this.cdRef.detectChanges(); // Manually trigger change detection
+            
+            
         } else {
           this.isStepDetected = false;
           this.cdRef.detectChanges(); // Manually trigger change detection
@@ -41,11 +48,16 @@ export class MotionComponent implements OnInit, OnDestroy {
   }
 
   resetSteps() {
-    this.stepCount = 0;
-    this.motionData.stepCount = 0;
-    this.cdRef.detectChanges();
-    console.log('Steps reset to zero.');
-  }
+      console.log('resetSteps() called');
+      // Set the isCounting flag to false to stop incrementing the steps
+      this.isCounting = false;
+      // Reset stepCount and motionData.stepCount to zero
+      this.stepCount = 0;
+      this.motionData.stepCount = 0;
+      // Trigger change detection to update the view
+      this.cdRef.detectChanges();
+      
+    }
   private delay(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
